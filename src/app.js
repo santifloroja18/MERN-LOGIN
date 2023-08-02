2878f6a8ed7fa71e18f56965ac5da04507d754d7
santifloroja18 <133369865+santifloroja18@users.noreply.github.com>
2023-08-02 10:22:19 -0500

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import morgan from "morgan";
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(cookieParser());
 app.use("/api", authRoutes);
 
 export default app;
